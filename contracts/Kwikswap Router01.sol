1d46a79a3f70700fc463c075d8819c730497ede3
KwikSwap <73940078+kwikswap@users.noreply.github.com>
2021-06-26 02:13:16 +0500

    Storing the Pair Address in Local variable

--- contracts/Kwikswap Router01.sol
+++ contracts/Kwikswap Router01.sol
@@ -394,9 +394,10 @@ contract KwikswapV1Router01 is IKwikswapV1Router01 {
         uint amountAMin,
         uint amountBMin
     ) private returns (uint amountA, uint amountB) {
+        address pair;
         // create the pair if it doesn't exist yet
         if (IKwikswapV1Factory(factory).getPair(tokenA, tokenB) == address(0)) {
-            IKwikswapV1Factory(factory).createPair(tokenA, tokenB);
+            pair = IKwikswapV1Factory(factory).createPair(tokenA, tokenB);
         }
         (uint reserveA, uint reserveB) = KwikswapV1Library.getReserves(factory, tokenA, tokenB);
         if (reserveA == 0 && reserveB == 0) {
